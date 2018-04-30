93396249b74eb415a16a8681d4ddfefd01c8ffc9
achen0063 <1176068637@qq.com>
2018-04-30 10:03:47 +0800

    Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.

--- js/main.js
+++ js/main.js
@@ -6,73 +6,104 @@ pageSize(yyy)
 listenMouse(yyy)
 
 var usingEraser = false
-eraser.onclick = function(){
+eraser.onclick = function () {
   usingEraser = true
-   btx.className = 'btx x'
+  btx.className = 'btx x'
 }
-brush.onclick = function(){
+brush.onclick = function () {
   usingEraser = true
   btx.className = 'btx'
 }
 
 
-function listenMouse(canvas){
+
+
+
+function listenMouse(canvas) {
   var using = false;
-  var lastPoint = {x:undefined,y:undefined}
-canvas.onmousedown = function(aaa){ 
-  using= true; 
-  var x = aaa.clientX;
-  var y = aaa.clientY;
-  if(usingEraser){
-    context.clearRect(x-5,y-5,10,10)
-  }
-  else{
-   lastPoint = {'x':x,'y':y}
-  }
-}
+  var lastPoint = { x: undefined, y: undefined }
+  if (document.body.ontouchstart !== undefined) {
 
-canvas.onmousemove = function(aaa){
-  var x = aaa.clientX;
-  var y = aaa.clientY;
-  if(!using){return}
-  if(usingEraser){
-    context.clearRect(x-5,y-5,10,10) 
-  }
-  else{
-    var newPoint = {'x':x,'y':y}
-    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-    lastPoint = newPoint  
-
+    canvas.ontouchstart = function (aaa) {
+      using = true;
+      var x = aaa.touches[0].clientX;
+      var y = aaa.touches[0].clientY;
+      if (usingEraser) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      }
+      else {
+        lastPoint = { 'x': x, 'y': y }
+      }
     }
-}
-canvas.onmouseup = function(aaa){
-   using = false;
-}
+    canvas.ontouchmove = function (aaa) {
+      var x = aaa.touches[0].clientX;
+      var y = aaa.touches[0].clientY;
+      if (!using) { return }
+      if (usingEraser) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      }
+      else {
+        var newPoint = { 'x': x, 'y': y }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    canvas.ontouchend = function () {
+      using = false;
+    }
+  }
+  else {
+    canvas.onmousedown = function (aaa) {
+      using = true;
+      var x = aaa.clientX;
+      var y = aaa.clientY;
+      if (usingEraser) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      }
+      else {
+        lastPoint = { 'x': x, 'y': y }
+      }
+    }
+
+    canvas.onmousemove = function (aaa) {
+      var x = aaa.clientX;
+      var y = aaa.clientY;
+      if (!using) { return }
+      if (usingEraser) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      }
+      else {
+        var newPoint = { 'x': x, 'y': y }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    canvas.onmouseup = function (aaa) {
+      using = false;
+    }
+  }
 }
 
-  
-
-
-function drawLine(x1,y1,x2,y2){
+function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.moveTo(x1,y1);
+  context.moveTo(x1, y1);
   context.lineWidth = 5;
-  context.lineTo(x2,y2);
+  context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
 }
-function pageSize(canvas){
+function pageSize(canvas) {
   page()
-  
-window.onresize = function(){
-  page()
-}
-function page(){
-  var pageWidth = document.documentElement.clientWidth;
-  var pageHeight = document.documentElement.clientHeight;
-  canvas.width = pageWidth;
-  canvas.height = pageHeight;
-}
-  
+
+  window.onresize = function () {
+    page()
+  }
+  function page() {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+  }
+
 }
 
